47856f183a1fcfce175e4a43e6f8c15d4881da08
alex <alex@platin.io>
2019-07-30 21:31:12 +0300

    Added simple service

---
+++ service.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
+}
+
+var msg int = rand.Intn(100)
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from: %d", msg)
+}
